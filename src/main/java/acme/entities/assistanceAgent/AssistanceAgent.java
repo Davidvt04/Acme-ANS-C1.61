e82f9ac87d7c6161f42459214502130b46a400ad
Mpino <marperdom@alum.us.es>
2025-03-04 13:20:29 +0100

    Creada la entidad  Claim

--- src/main/java/acme/entities/assistanceAgent/AssistanceAgent.java
+++ src/main/java/acme/entities/assistanceAgent/AssistanceAgent.java
@@ -20,7 +20,7 @@ import lombok.Setter;
 @Entity
 @Getter
 @Setter
-public class assistanceAgent extends AbstractEntity {
+public class AssistanceAgent extends AbstractEntity {
 
 	private static final long	serialVersionUID	= 1L;
 
