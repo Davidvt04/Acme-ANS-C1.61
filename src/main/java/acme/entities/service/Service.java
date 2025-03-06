baae7ac6de92e8b94594344b815fb68d65daa218
Ivo Raimondi <raimondiivo@gmail.com>
2025-03-06 19:38:50 +0100

    Terminada clase service con sus correspondientes rangos Refs #48

--- src/main/java/acme/entities/service/Service.java
+++ src/main/java/acme/entities/service/Service.java
@@ -3,14 +3,13 @@ package acme.entities.service;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.validation.Valid;
 
 import acme.client.components.basis.AbstractEntity;
-import acme.client.components.datatypes.Money;
 import acme.client.components.mappings.Automapped;
 import acme.client.components.validation.Mandatory;
 import acme.client.components.validation.Optional;
-import acme.client.components.validation.ValidMoney;
+import acme.client.components.validation.ValidNumber;
+import acme.client.components.validation.ValidScore;
 import acme.client.components.validation.ValidString;
 import acme.client.components.validation.ValidUrl;
 import lombok.Getter;
@@ -34,17 +33,17 @@ public class Service extends AbstractEntity {
 	private String				picture;
 
 	@Mandatory
-	@Valid //Preguntar
+	@ValidNumber(min = 1, max = 100, integer = 3, fraction = 2)
 	@Automapped
 	private Double				averageDwellTime;
 
 	@Optional
-	@ValidString(min = 7, max = 7, pattern = "^[A-Z]{4}-[0-9]{2}$")
+	@ValidString(pattern = "^[A-Z]{4}-[0-9]{2}$")
 	@Column(unique = true)
 	private String				promotionCode;
 
 	@Optional
-	@ValidMoney(min = 0, max = 10000) //Preguntar
+	@ValidScore
 	@Automapped
-	private Money				money;
+	private Double				discountMoney;
 }
