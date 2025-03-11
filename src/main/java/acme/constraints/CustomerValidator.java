faef30872037449c90889fb1fb815c117fdf63fb
David <davidvt04@gmail.com>
2025-03-11 21:32:15 +0100

    Añadido validador para el identificador del customer

--- src/main/java/acme/constraints/CustomerValidator.java
+++ src/main/java/acme/constraints/CustomerValidator.java
@@ -1,32 +1,37 @@
 
 package acme.constraints;
 
-import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
 import acme.client.components.principals.UserAccount;
+import acme.client.components.validation.AbstractValidator;
+import acme.client.components.validation.Validator;
 import acme.realms.Customer;
 
-public class CustomerIdentifierValidator implements ConstraintValidator<ValidCustomerIdentifier, String> {
-
-	private Customer customer;
-
+@Validator
+public class CustomerValidator extends AbstractValidator<ValidCustomer, Customer> {
 
 	@Override
-	public boolean isValid(final String value, final ConstraintValidatorContext context) {
-		if (value == null || value.isBlank() || !value.matches("^[A-Z]{2,3}\\\\d{6}$")) {
+	protected void initialise(final ValidCustomer annotation) {
+		assert annotation != null;
+	}
+
+	@Override
+	public boolean isValid(final Customer customer, final ConstraintValidatorContext context) {
+
+		if (customer.getIdentifier() == null || customer.getIdentifier().isBlank() || !customer.getIdentifier().matches("^[A-Z]{2,3}\\d{6}$")) {
 			context.disableDefaultConstraintViolation();
 			context.buildConstraintViolationWithTemplate("The identifier must not be null or blank and must follow the pattern").addConstraintViolation();
 			return false;
 		}
 
-		if (this.customer == null) {
+		if (customer == null) {
 			context.disableDefaultConstraintViolation();
 			context.buildConstraintViolationWithTemplate("Customer must not be null").addConstraintViolation();
 			return false;
 		}
 
-		UserAccount userAccount = this.customer.getUserAccount();
+		UserAccount userAccount = customer.getUserAccount();
 		if (userAccount == null || userAccount.getIdentity() == null) {
 			context.disableDefaultConstraintViolation();
 			context.buildConstraintViolationWithTemplate("User Account and Identity must not be null").addConstraintViolation();
@@ -45,22 +50,22 @@ public class CustomerIdentifierValidator implements ConstraintValidator<ValidCus
 		}
 
 		String nombre = userAccount.getIdentity().getName();
-		String apellidos = userAccount.getIdentity().getSurname();
+		String[] apellidos = userAccount.getIdentity().getSurname().split(" ");
 		String inicialNombre = String.valueOf(nombre.charAt(0)).toUpperCase();
-		String inicial1Apellido = String.valueOf(apellidos.charAt(0)).toUpperCase();
+		String inicial1Apellido = String.valueOf(apellidos[0].charAt(0)).toUpperCase();
 		String inicial2Apellido = "";
 		int initialsLenght = 2;
-		if (apellidos.contains(" ")) {
-			inicial2Apellido = String.valueOf(apellidos.split(" ")[1].charAt(0)).toUpperCase();
+		if (apellidos.length > 1) {
+			inicial2Apellido = String.valueOf(apellidos[1].charAt(0)).toUpperCase();
 			initialsLenght = 3;
 		}
 		String iniciales = inicialNombre + inicial1Apellido + inicial2Apellido;
 
-		String identifierInitials = value.substring(0, initialsLenght);
+		String identifierInitials = customer.getIdentifier().substring(0, initialsLenght);
 
 		if (!iniciales.equals(identifierInitials)) {
 			context.disableDefaultConstraintViolation();
-			context.buildConstraintViolationWithTemplate("Identifier must start with initials of the user").addConstraintViolation();
+			context.buildConstraintViolationWithTemplate("Identifier must start with initials of the user: Should be " + identifierInitials + " but is " + iniciales).addConstraintViolation();
 			return false;
 		}
 		return true;
